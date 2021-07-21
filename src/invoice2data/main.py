443f7086894bde544b13f1e3754a8d57b9130b23
Javi Vindas <jsolis@akamai.com>
2021-07-21 09:37:10 -0600

    test submodule

--- src/invoice2data/main.py
+++ src/invoice2data/main.py
@@ -79,6 +79,9 @@ def extract_data(invoicefile, templates=None, input_module=pdftotext):
         templates = read_templates()
 
     # print(templates[0])
+
+    print('this is a submodule')
+
     extracted_str = input_module.to_text(invoicefile).decode("utf-8")
 
     logger.debug("START pdftotext result ===========================")
